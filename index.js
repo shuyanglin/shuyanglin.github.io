16eb33f530685bdc867315d0bda5ffc727e86a65
shuyanglin <shuyanglin.tw@gmail.com>
2021-05-02 02:15:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 
 app.use(express.static('public')); 
-app.listen(8000, () => console.log("listening at port 8000."));
+app.listen(8001, () => console.log("listening at port 8001."));
 
 app.get('/api', function (request, response) {
-    console.log("get request");
-    response.end();
+    console.log("got request");
+    var km = {
+        ride: 1000,
+        run: 5000
+    }
+    response.json({
+        message: "status ok",
+        km: km
+    })
 });
